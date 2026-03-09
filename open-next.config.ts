4c072fa048f3b2b175b34d6fc9147a36f9172ae9
Koravit Kessaro <koravit.kessaro.pgm@gmail.com>
2026-03-09 23:03:12 +0700

    add open-next

---
+++ open-next.config.ts
@@ -0,0 +1,21 @@
+const config = {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
+};
+
+export default config;
